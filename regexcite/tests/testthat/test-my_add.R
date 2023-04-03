6d397e1f3af64c82b0a95e03bf84655cb6f24679
NingWang0123 <1198137403wn@gmail.com>
2023-04-03 02:15:24 -0700

    update files

--- regexcite/tests/testthat/test-my_add.R
+++ regexcite/tests/testthat/test-my_add.R
@@ -1,3 +1,27 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("addition works", {
+  expect_equal(my_add(1,2),3)
+})
+
+test_that("the addition for default works", {
+  expect_equal(my_add(7),17)
+})
+
+test_that("the addition for x is NA and y is default works", {
+  expect_true(is.na(my_add(NA)))
+})
+
+test_that("the addition for x is NA works", {
+  expect_true(is.na(my_add(NA, 3)))
+})
+
+test_that("the addition for y is NA works", {
+  expect_true(is.na(my_add(7, NA)))
+})
+
+test_that("the addition for x is string works", {
+  expect_error(my_add("as", NA), "One of your inputs contains a string value")
+})
+
+test_that("the addition for y is string works", {
+  expect_error(my_add(7, "basd"), "One of your inputs contains a string value")
 })
